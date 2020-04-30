a56fbbe26431f514fbcc94c011e11842d7e8fa19
Tyler Bolty <tsbolty@gmail.com>
2020-04-30 17:04:46 -0600

    Added JAWSDB_URL to connection file

--- config/connection.js
+++ config/connection.js
@@ -8,14 +8,19 @@ var PORT = process.env.PORT || 8080;
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+var connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Gladiator5972!",
-    port: 3306,
-    database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Gladiator5972!",
+        port: 3306,
+        database: "burgers_db"
+    });
+};
 
 connection.connect(err => {
     if (err) throw err
