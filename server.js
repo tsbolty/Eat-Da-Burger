99cd0685733fb038947a97f602c53f4f3dcd2812
Tyler Bolty <tsbolty@gmail.com>
2020-04-30 18:14:14 -0600

    trying to push to heroku

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burger_controller.js");
 
+
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
